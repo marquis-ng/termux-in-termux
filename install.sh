1aa852ea31b19de871560f1dbfdd903066411193
Marquis <59020070+marquis-ng@users.noreply.github.com>
2022-10-30 17:38:28 +0800

    Exit install.sh when command returns non-zero exit code

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 run() {
 	printf "\033[32m+ \033[34m%s\033[0m\n" "$*"
-	if "$@"; then
-		return 0
-	else
-		return 1
+	if ! "$@"; then
+		printf "\033[1;31mCommand returned an error. Abort.\n" 1>&2
+		exit 1
 	fi
 }
 
@@ -15,13 +14,13 @@ msg() {
 installer() {
 	msg "Installing files to system..."
 	run mkdir -p "$PREFIX/bin"
-	run cp -f tit "$PREFIX/bin/tit"
+	run cp -f "$BASE/tit" "$PREFIX/bin/tit"
 	run chmod +x "$PREFIX/bin/tit"
 	run mkdir -p "$PREFIX/etc/bash_completion.d"
-	run cp -f tit-completion.bash "$PREFIX/etc/bash_completion.d/tit-completion.bash"
+	run cp -f "$BASE/tit-completion.bash" "$PREFIX/etc/bash_completion.d/tit-completion.bash"
 	run mkdir -p "$PREFIX/share/doc/tit"
-	run cp -f README.md "$PREFIX/share/doc/tit/README.md"
-	msg "Done. Run 'tit-- help' to start using Termux-in-Termux."
+	run cp -f "$BASE/README.md" "$PREFIX/share/doc/tit/README.md"
+	msg "Done. Run 'tit --help' to start using Termux-in-Termux."
 }
 
 uninstaller() {
@@ -35,7 +34,7 @@ uninstaller() {
 
 # shellcheck disable=SC2155
 export PREFIX="$(realpath -- "${PREFIX:-/usr}")"
-cd "$(realpath -- "$(dirname -- "$0")")" || exit
+BASE="$(realpath -- "$(dirname -- "$0")")"
 case "$1" in
 	install)
 		installer
