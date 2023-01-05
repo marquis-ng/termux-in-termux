fbc725443fb47edfe8b57d239707c910d80e3fd2
tovicheung <discordpto1227@gmail.com>
2023-01-05 21:04:51 +0800

    removed ANSI color codes removed character "✓" removed similarity check (unnecessary) added alias

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 run() {
-	printf "\033[32m+ \033[34m%s\033[0m\n" "$*"
+	printf "RUN: %s\n" "$*"
 	if ! "$@"; then
-		printf "\033[1;31mCommand returned an error. Abort.\n" 1>&2
+		printf "ERR\n" 1>&2
 		exit 1
 	fi
 }
 
-msg() {
-	printf "\033[1;36m%s\033[0m\n" "$@"
-}
-
 installer() {
-	msg "Installing files to system..."
+	echo "Installing files to system..."
 	run mkdir -p "$PREFIX/bin"
 	run cp -f "$BASE/tit" "$PREFIX/bin/tit"
 	run chmod +x "$PREFIX/bin/tit"
@@ -20,16 +16,16 @@ installer() {
 	run cp -f "$BASE/tit-completion.bash" "$PREFIX/etc/bash_completion.d/tit-completion.bash"
 	run mkdir -p "$PREFIX/share/doc/tit"
 	run cp -f "$BASE/README.md" "$PREFIX/share/doc/tit/README.md"
-	msg "Done. Run 'tit --help' to start using Termux-in-Termux."
+	echo "Done. Run 'tit --help' to start using Termux-in-Termux."
 }
 
 uninstaller() {
-	msg "Reverting changes to system..."
+	echo "Reverting changes to system..."
 	run rm -rf "$PREFIX/bin/tit"
 	run rm -rf "$PREFIX/etc/bash_completion.d/tit-completion.bash"
 	run rm -rf "$PREFIX/share/doc/tit/README.md"
 	run rm -rf "$HOME"/termux{,-pacman}-fs{,32}
-	msg "Done."
+	echo "Done."
 }
 
 # shellcheck disable=SC2155
@@ -43,6 +39,6 @@ case "$1" in
 		uninstaller
 		;;
 	*)
-		msg "Usage: $0 [install | uninstall]"
+		echo "Usage: $0 [install | uninstall]"
 		;;
 esac
